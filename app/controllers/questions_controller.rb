c32a2c49edf2b51b7eef7bc9b97957127ecb12fd
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-16 18:38:57 +0800

    [Stella]Enable application to read route through url

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,10 @@
 class QuestionsController < ApplicationController
+    def show
+        @question = Question.find(params[:id])
+    end
+
     def create
         Question.create(questions_params)
-
         redirect_to root_path
     end
 
