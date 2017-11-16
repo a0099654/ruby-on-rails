d600e36479c155eb2ffe1e9620ad496352071194
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-16 18:18:52 +0800

    [Stella]Update route file, remove the newly created questions file and hashing the data obtain from questions form

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do 
-  get 'questions/show'
-
+ 
   resources :questions
   root 'home#index'
   
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/questions' => 'home#index'
-  post '/answer' => 'home#index'
 
-  get '/questions/:id' => 'home#question'
 end
