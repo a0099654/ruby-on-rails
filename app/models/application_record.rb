7ffa9caab2fe5172f6a4a14cb065c1ff348aae25
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-16 17:25:46 +0800

    [Stella]Debug the application to enable user to go to /questions/{id} and remove question.html.erb and try to implement md5 for the avatar

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  def gravatar
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do 
+  get 'questions/show'
+
   resources :questions
   root 'home#index'
   
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/questions' => 'home#temp'
-  post '/answer' => 'home#temp'
+  post '/questions' => 'home#index'
+  post '/answer' => 'home#index'
 
   get '/questions/:id' => 'home#question'
 end
