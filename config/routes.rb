2afd3a819f07efa9a4cc1f0e44266c998dd4810a
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-16 16:09:43 +0800

    [Stella]Edit the end point for form of the new answer and modify the route to the temporary route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/questions' => 'home#temp'
+  post '/answer' => 'home#temp'
 
   get '/questions/:id' => 'home#question'
 end
