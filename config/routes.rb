1066ceaceb8bca2f014151dbba00d23669e4618f
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-16 14:58:23 +0800

    [Stella]Add route for posting questions and getting invalid authorisation problem

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/questions' => 'home#temp'
 end
