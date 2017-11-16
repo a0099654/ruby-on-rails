01d7155a07c99b52003135f18947c4f782c42bb1
Stella <stella.widyasari@gmail.com>
2017-11-17 00:20:53 +0800

    [Stella]Update database schema and create answer controller to handle the pages

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,12 @@
 class AnswersController < ApplicationController
+  def create
+      Answer.create(answers_params)
+      redirect_to root_path
+  end
+
+  private
+
+  def answers_params
+      params.require(:answer).permit(:question_id, :email, :body)
+  end
 end
