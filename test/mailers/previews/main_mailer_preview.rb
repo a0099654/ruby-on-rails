6622f8ab8f46f42dc80414f9971ece997e218158
Stella <stella.widyasari@gmail.com>
2017-11-19 17:49:29 +0800

    [Stella]Implement method to send email to author once someone answer and edit the preview email

--- test/mailers/previews/main_mailer_preview.rb
+++ test/mailers/previews/main_mailer_preview.rb
@@ -3,7 +3,12 @@ class MainMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/main_mailer/notify_question_author
   def notify_question_author
-    MainMailer.notify_question_author
+    question = Question.create email:'author@gmail.com', body:'this is a question'
+    answer = Answer.create email:'authorAnswer@gmail.com', body:'this is an answer'
+
+    question.answers << answer
+    
+    MainMailer.notify_question_author(answer)
   end
 
 end
