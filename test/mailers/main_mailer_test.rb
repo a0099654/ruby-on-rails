dc1d274a6eb2918635d745afaf6cef8717160ce8
Stella <stella.widyasari@gmail.com>
2017-11-19 17:40:44 +0800

    [Stella]Create main mailer test, set the string in locale file, and implement email functionality

--- test/mailers/main_mailer_test.rb
+++ test/mailers/main_mailer_test.rb
@@ -2,11 +2,21 @@ require 'test_helper'
 
 class MainMailerTest < ActionMailer::TestCase
   test "notify_question_author" do
-    mail = MainMailer.notify_question_author
-    assert_equal "Notify question author", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+
+    #Create a question and answer sample
+    question = Question.create email:'author@gmail.com', body:'this is a question'
+    answer = Answer.create email:'authorAnswer@gmail.com', body:'this is an answer'
+
+    #Associate answer to the question by appending it
+    question.answers << answer
+
+    mail = MainMailer.notify_question_author(answer)
+    assert_equal "New answer", mail.subject
+
+    assert_equal [question.email], mail.to
+    assert_equal [answer.email], mail.from
+    
+    assert_match answer.body, mail.body.encoded
   end
 
 end
